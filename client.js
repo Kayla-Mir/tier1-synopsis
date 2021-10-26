b4a47adad928aa5a43b5e82d3041784f9d916c2c
Kayla Mir <kayla.mir32@gmail.com>
2021-10-26 15:30:00 -0500

    laid the bare bones for the webpage

---
+++ client.js
@@ -0,0 +1,22 @@
+$(document).ready(readyNow);
+
+const budget = 25000;
+let purchases = [];
+
+function addPurchase() {
+  console.log('in addPurchase');
+  let newPurchase = {
+    name: $('#purchaseNameIn').val(),
+    price: $('#purchasePriceIn').val()
+  }
+  purchases.push(newPurchase);
+  $('#purchaseNameIn').val('');
+  $('#purchasePriceIn').val('');
+}
+
+function readyNow() {
+  let el = $('#budgetOut');
+  el.empty();
+  el.append(budget);
+  $('#addPurchaseButton').on('click', addPurchase);
+}
